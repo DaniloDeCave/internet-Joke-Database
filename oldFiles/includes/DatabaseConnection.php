6a59e683d6e936cca5addd3175d458484c7e7f80
MajorGrom <danilo.decave@outlook.it>
2022-01-12 17:46:00 +0100

    rimossa cartella oldfile

--- oldFiles/includes/DatabaseConnection.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $pdo = new PDO('mysql:host=localhost;dbname=ijdb','root','');
-    // ATTR_ERRMODE costante della classe PDO che controlla la modalita di errore
-    // ERRMODE_EXCEPTION permette impostare la modalitá che lancia eccezioni
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
--- oldFiles/templates/addjoke.html.php
+++
@@ -1,5 +0,0 @@
-<form action="" method="post">
-    <label for="joketext">Type your Joke here :</label>
-    <textarea name="joketext" id="joketext" cols="40" rows="3"></textarea>
-    <input type="submit" name="submit" value="Add">
-</form>
